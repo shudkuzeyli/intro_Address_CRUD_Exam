25402a751908f27b92d06efb30421380bd21b77e
KediBedri <hudkuzeyli@gmail.com>
2025-01-12 01:42:18 +0300

    -

--- Context/DataContext.cs
+++ Context/DataContext.cs
@@ -4,16 +4,17 @@ using System.Diagnostics.Metrics;
 
 namespace intro_Address_CRUD_Exam.Context
 {
-	public class DataContext:DbContext
+	public class DataContext : DbContext
 	{
-	public DataContext(DbContextOptions <DataContext> options) : base (options) 
-	{ 
-	
-	}
 
-	public DbSet<Country> Country { get; set; }
-	public DbSet<Address> Address { get; set; }
-	public DbSet<City> City { get; set; }	
-	public DbSet<Ilce> Ilce { get; set; }
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Country> Country { get; set; }
+		public DbSet<Address> Address { get; set; }
+		public DbSet<City> City { get; set; }
+		public DbSet<Ilce> Ilce { get; set; }
 	}
 }
